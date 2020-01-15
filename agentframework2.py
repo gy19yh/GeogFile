8d3ba68a7353890e3bc398b42ee43fa8afb5eae9
gy19yh <56163822+gy19yh@users.noreply.github.com>
2020-01-15 20:37:13 +0000

    Add files via upload

--- agentframework2.py
+++ agentframework2.py
@@ -12,26 +12,30 @@ class Agent():
         self.x = x
         self.y = y
         self.z = z
-    
+    # Bacteria move horizontally by the influence of wind
+    # defined east in the x coorderate and north in the y coorderate as positive
     def Hori(self) :
         rate = random.random()
-        if rate <= 0.05:
-            self.x = (self.x - 1) 
-        elif 0.05 < rate <= 0.15: 
-            self.y = (self.y - 1) 
-        elif 0.15 < rate <= 0.25:
-            self.y = (self.y + 1) 
-        else: 
-            self.x = (self.x + 1)
-       
+        if rate <= 0.05:          # there is 5% chance
+            self.x = (self.x - 1) # the bateria will blow to west 1 meter
+        elif 0.05 < rate <= 0.15: # there is 10% chance
+            self.y = (self.y - 1) # the bateria will blow to south 1 meter
+        elif 0.15 < rate <= 0.25: # there is 10% chance
+            self.y = (self.y + 1) # the bateria will blow to north 1 meter
+        else:                     # there is 75% chance  
+            self.x = (self.x + 1) # the bateria will blow to east 1 meter
+    
+    # Bateria move vertial
+    # defined up as positive way and down as the negative way
     def Vert(self):
-        if self.z > 75:
+        if self.z > 75:                 # check whether the weapon above 75 meters high
             rate = random.random()
-            if rate <= 0.20:
-                self.z += 1
-            elif 0.20 < rate <= 0.30:
-                self.z = self.z
-            else: 
-                self.z -= 1
+            if rate <= 0.20:            # there is 20% chance
+                self.z += 1             # the bateria will rise 1 meter by turbulance
+            elif 0.20 < rate <= 0.30:   # there is 10% chance
+                self.z = self.z         # the bateria will stay at same level
+            else:                       # there is 70% chance
+                self.z -= 1             # the bateria will drop 1 meter
         else:
-            self.z -= 1
+            self.z -= 1                 # if the height of weapon below 75 meters it will not influence by turbulance
+                                        # the bateria will drop 1 meter a second
--- assessment2.py
+++ assessment2.py
@@ -17,16 +17,17 @@ import matplotlib.pyplot
 
 #num_of_agents = 5000
 
-###### allow user to set the number of bateria and check whether is digit or not ###
+# allow user to set the number of bateria and check whether is digit or not 
 chance = 3
 while True:
     num_of_agents = input('How much bateria You prefer to lease:')
-    
+# give user 3 times to input the right format#    
     if chance > 1:
-        
+        # check the format of input
         if num_of_agents.isdigit():
             break
-    
+        # if the user input the wrong format, give the another chance to input the number
+        # However, the user only have 3 times to input the number of bateria
         else:
             print('You must input number! Without any letter and symbol!') 
             print('You still have', chance-1,'chances')
@@ -34,14 +35,21 @@ while True:
             continue
     
     else:
+         # if the user run out of all chances and still input the wrong format
+         # the programme will be shutting down
          print('You run out of all opportunities')
-         raise SystemExit 
+         raise SystemExit # the code aims to shut down the programme
          
 
 temp = int(num_of_agents)  
 num_of_iterations = 500 
 agents = []
+
+# the height of the building is 75m
+# the bateria is above the top of the building. But we not sure how height it is.
+# Hence, I set the random height of the weapon 
 z = random.randint(75,150)
+
 #print(z)
 
 ######### step 1: Pull in the data file   #########
@@ -59,16 +67,17 @@ with open('wind.raster.txt', newline='') as csvfile: # open the data file named 
 items = 0
 for row in environment:
     for value in row:
-        if value != 0:
+        if value > 0:
             x = row.index(value)
             y = items
               
     items += 1
-     
+# after locating the resource of the weapon, mark it as a red star    
 matplotlib.pyplot.scatter(x, y, marker='*',color='red')
 
 
 ##### step 2: where bacteria will end up #####
+# link the model with the agentframework2 
 for i in range(temp):
     agents.append(agentframework2.Agent(x,y,z))
       
@@ -78,28 +87,38 @@ for i in range(temp):
         agents[i].Vert()       
  
 ##### step 3: Draws a density map of where all the bacteria end up as an image and displays it on the screen ####    
+# after bateria falling on the environment, the value of the certain area plus 10, which makes it more visable
+# this is the core of the code to make the density map
 for i in range(temp):   
     environment[agents[i].y][agents[i].x] += 10
-    #matplotlib.pyplot.scatter(agents[i].x,agents[i].y,marker='.')
-      
+    #matplotlib.pyplot.scatter(agents[i].x,agents[i].y,marker='.') 
+
+# draw a map with the 300*300 size      
 matplotlib.pyplot.ylim(0, 300)
 matplotlib.pyplot.xlim(0, 300)
-matplotlib.pyplot.imshow(environment)
+matplotlib.pyplot.imshow(environment) # shows the environment
 matplotlib.pyplot.show()
+
+# to show the location of the weapon
 print('Weapon located at the STAR point')
+# to show the height of the weapon
 print('The altitude of resource is', z,'meters')
 
 ##### step 4: write the density map into txt file##########
 output=[]
+# input the title of each row
 output.append('X'+'Y')
+# input the value of each pair of coordinate
 for i in range(temp): 
     location=[]
     location.append(agents[i].y)
     location.append(agents[i].x)
     output.append(location)  
-    
+
+# create the text file and write the value in 
 f = open('location.txt', 'w', newline='')
 writer = csv.writer(f, delimiter=',')
 for i in output:
     writer.writerow(i)
 f.close()
+
